fa76f4d9890e4c7f12312ba931b851ddea16a439
chain13 <chain12341@outlook.com>
2025-07-11 15:57:20 +0700

    add preview setting

---
+++ config/settings/preview.py
@@ -0,0 +1,10 @@
+from .base import *
+from dotenv import load_dotenv
+load_dotenv()
+#noqa
+DEBUG = False
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",") + ["127.0.0.1"]
+SECRET_KEY = os.getenv("SECRET_KEY")
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
